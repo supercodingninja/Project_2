960581ee3fe72d5ce3daf0be8d9b7aa12b14f3bf
SCN <supercodingninja@outlook.com>
2021-01-30 21:22:49 -0800

    first try

--- models/index.js
+++ models/index.js
@@ -5,13 +5,14 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
+var config    = require(__dirname + '/../config/config.js');
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env[config.production.use_env_variable]) {
+  var sequelize = new Sequelize(process.env[config.production.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.development.database, config.development.username, 
+    config.development.password, config.development);
 }
 
 fs
